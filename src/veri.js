380dce516db5471743f01db2d250f1b3b78d465e
Okan Baş <okan@hub.studio>
2023-03-26 16:30:36 +0300

    feat: kitap listesi örneği yapıldı

---
+++ src/veri.js
@@ -0,0 +1,7 @@
+const kitaplar = [
+  { ad: 'Kitap adı 1', yazar: 'Yazar 1', fiyat: 58.9 },
+  { ad: 'Kitap adı 2', yazar: 'Yazar 2', fiyat: 68.9 },
+  { ad: 'Kitap adı 3', yazar: 'Yazar 3', fiyat: 38.9 },
+];
+
+export default kitaplar;
