633178f4e61a8722d4cbcf1b62ab950fbe66e35a
Okan Baş <okan@hub.studio>
2023-04-04 09:33:44 +0300

    feat:kisi liste veri map örnegi yapıldı

---
+++ src/Kisi.jsx
@@ -0,0 +1,15 @@
+function Kisi({ veri }) {
+  let dogumTarihi = new Date(veri.dob.date).getFullYear();
+
+  return (
+    <div className="col-md-3">
+      <img src={veri.picture.medium} alt="" />
+      <p>
+        {veri.name.first} {veri.name.last}
+      </p>
+      <p>{dogumTarihi}</p>
+    </div>
+  );
+}
+
+export default Kisi;
---
+++ src/KisiListe.jsx
@@ -0,0 +1,16 @@
+import kisiler from './veri';
+import Kisi from './Kisi';
+
+function KisiListe() {
+  return (
+    <>
+      <div className="row">
+        {kisiler.map((kisi, index) => {
+          return <Kisi veri={kisi} key={index} />;
+        })}
+      </div>
+    </>
+  );
+}
+
+export default KisiListe;
